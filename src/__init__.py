b325a03d1f2249bcb9df6a820236d673e49adedd
Kevin <kevinagredojara@outlook.com>
2026-01-11 18:34:12 -0500

    Inicializar paquete src
    Add module documentation and metadata

---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+"""
+DengueClima - Sistema de Predicción de Dengue basado en Variables Climáticas
+
+Este paquete contiene los módulos principales del proyecto:
+- data_processing: Funciones para carga y procesamiento de datos
+- model: Modelos de predicción de casos de dengue
+- utils: Funciones auxiliares
+"""
+
+__version__ = "0.1.0"
+__author__ = "Kevin Agredo Jara"
